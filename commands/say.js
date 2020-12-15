78c567849ea79f5fbded418b70bf51d294bad776
richen604 <56615615+richen604@users.noreply.github.com>
2020-12-15 09:14:31 -0800

    added /commands/say.js

---
+++ commands/say.js
@@ -0,0 +1,19 @@
+module.exports = {
+  name: "bork",
+  description:
+    "takes given message and says it through the bot in a given channel",
+  usage: "<channel> <message> || <channel.id> <message>",
+  cooldown: "5",
+  guildOnly: "true",
+  execute(client, message, args) {
+    const channel = message.guild.channels.cache.get(args[0]) || args[0];
+    if (!channel) message.reply("Please include a valid channel or channel id");
+
+    const sayMessage = args.slice(1, args.length).slice(",").join(" ");
+    if (!sayMessage) message.reply("Please include a message with channel");
+
+    channel.send(sayMessage).catch((err) => {
+      console.log("Error with sending message to channel in say command", err);
+    });
+  },
+};
