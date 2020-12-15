2c3de4c31c2f642dc86a2ae9b75d3f0164cc9908
richen604 <56615615+richen604@users.noreply.github.com>
2020-12-15 09:47:30 -0800

    fixed say command bug with args

--- commands/say.js
+++ commands/say.js
@@ -6,15 +6,24 @@ module.exports = {
   cooldown: "5",
   guildOnly: "true",
   execute(client, message, args) {
-    const channel = message.guild.channels.cache.get(args[0]) || args[0];
-    if (!channel) message.reply("Please include a valid channel or channel id");
+    const channel =
+      message.guild.channels.cache.get(args[0]) ||
+      message.guild.channels.cache.get(
+        args[0].replace("<#", "").replace(">", "")
+      );
+    console.log("channel is", channel);
+    if (!channel)
+      return message.reply("Please include a valid channel or channel id");
 
     const sayMessage = args.slice(1, args.length).slice(",").join(" ");
-    if (!sayMessage) message.reply("Please include a message with channel");
+    if (!sayMessage)
+      return message.reply("Please include a message with channel");
 
     channel.send(sayMessage).catch((err) => {
-      console.log("Error with sending message to channel in say command", err);
+      return console.log(
+        "Error with sending message to channel in say command",
+        err
+      );
     });
-    return;
   },
 };
